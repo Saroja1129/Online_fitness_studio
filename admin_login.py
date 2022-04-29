74add9bfd64a3e9cc4b763abd1444e9db0143d69
vikastadepu <vikastadepuffe@gmail.com>
2022-04-29 12:02:23 -0700

    changes
    Changed links to register page,
    instructor page

--- admin_login.py
+++ admin_login.py
@@ -9,7 +9,7 @@ from hashlib import pbkdf2_hmac # for hash function
 from PIL import *
 
 #Change here for your local DB password
-local_DB_Password = "Arti@123"
+local_DB_Password = "password"
 python_alias = "python3"
 
 
@@ -42,7 +42,7 @@ def submitact():
     if(user=="" or passw==""):
         msgbox.showinfo("Insert status","all fields are required")
     elif(a=='Admin'):
-        # Update user and password
+     
         con=mysql.connect(host="localhost",user="root",password=local_DB_Password, db="fitnessstudio") 
         cursor=con.cursor()
         cursor.execute("select * from admin where admin_email = %s and admin_pwd=%s", [(user),(passw)] )
@@ -108,43 +108,46 @@ def submitact():
             
 Admin=tk.Tk()
 Admin.title("Fitness_Studio")
-Admin.geometry("800x600")
+Admin.geometry("900x900")
+
+Label_IH = tk.Label(Admin, text ="Welcome To Onilne Fitness Studio" )
+Label_IH.config(font=("Courier", 30))
+Label_IH.place(x = 30, y = 20)
 
 Label_Domain = tk.Label(Admin, text ="Domain ", font=('bold',10) )
-Label_Domain.place(x = 50, y = 20)
+Label_Domain.place(x = 200, y = 210)
 
 clicked=StringVar()
 clicked.set("Admin")
 drop = OptionMenu( Admin , clicked, "Admin","Instructor","Client","Advisor")
-drop.place(x = 150, y = 20, width = 200)
+drop.place(x = 300, y = 210, width = 200)
 
-# Domain = tk.Entry(Admin, width = 35)
-# Domain.place(x = 150, y = 20, width = 200)
+
 
 Label_User_name = tk.Label(Admin, text ="UserName ", )
-Label_User_name.place(x = 50, y = 60)
+Label_User_name.place(x = 200, y = 280)
 
 Username = tk.Entry(Admin, width = 35)
-Username.place(x = 150, y = 60, width = 200)
+Username.place(x = 300, y = 280, width = 200)
 
 Label_Password = tk.Label(Admin, text ="Password ", )
-Label_Password.place(x = 50, y = 100)
+Label_Password.place(x = 200, y = 350)
 
 Password= tk.Entry(Admin, width = 35)
-Password.place(x = 150, y = 100, width = 200)
+Password.place(x = 300, y = 350, width = 200)
 Password.config(show="*")
 
 
 submitbtn = tk.Button(Admin, text ="Login",
                       bg ='blue', command=submitact)
-submitbtn.place(x = 200, y = 140, width = 55)
+submitbtn.place(x = 350, y = 420, width = 55)
 
 Label4 = tk.Label(Admin, text ="If you are new client", )
-Label4.place(x = 150, y = 200)
+Label4.place(x = 270, y = 500)
 
 submitbtn1 = tk.Button(Admin, text ="Register Here",
                       bg ='silver', command=Register)
-submitbtn1.place(x = 280, y = 200, width = 100)
+submitbtn1.place(x = 400, y = 500, width = 100)
 
 Admin.mainloop()
 
