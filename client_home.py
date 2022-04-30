09fdf96bbab35ab110b060a3af3db7739c472036
VineetKiragi <37727735+VineetKiragi@users.noreply.github.com>
2022-04-30 15:14:56 -0700

    Update client_home.py
    This page is just for testing the code with jasmine. please dont use this

--- client_home.py
+++ client_home.py
@@ -58,6 +58,13 @@ def train_sessions(kind):
             
         i=i+1
     Rec_tra_sessions.mainloop()
+    
+def training_sessions1():
+    Client.destroy()
+    #call(["python","Premium.py"])
+    #call([python_alias,"test.py","--input", user, "--pw", local_DB_password,"--Job",a])
+    call([python_alias,"training_session.py  ","--input", user, "--pw"])
+    print("training")
 
 def fitness_seminars(kind):
     Rec_fitness_seminars = tk.Tk()
@@ -175,11 +182,12 @@ if(Type=="Free"):
     kind='recorded'
     num1 = tk.Label(Client, text ="1.", font=('bold',20) )
     num1.place(x = 30, y = 90)
-    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    #submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=training_sessions1)
     submitbtn1.place(x = 50, y = 90, width = 200)
     
     #access to recorded fitness seminars page
-    #kind='recorded'
+    kind='recorded'
     num2 = tk.Label(Client, text ="2.", font=('bold',20) )
     num2.place(x = 30, y = 120)
     submitbtn2 = tk.Button(Client, text =kind+" Fitness Seminars",bg ='silver', command=lambda:fitness_seminars(kind='recorded'))
@@ -204,13 +212,15 @@ elif(Type=="Basic"):
     Output_free.place(x = 30, y = 60)
     
     #access to recorded training sessions page
-    kind='recorded'
+    #kind='recorded'
     num1 = tk.Label(Client, text ="1.", font=('bold',20) )
     num1.place(x = 30, y = 90)
-    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    #submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=training_sessions1)
     submitbtn1.place(x = 50, y = 90, width = 200)
     
     #access to recorded fitness seminars page
+    kind='recorded'
     num2 = tk.Label(Client, text ="2.", font=('bold',20) )
     num2.place(x = 30, y = 120)
     submitbtn2 = tk.Button(Client, text =kind+" Fitness Seminars",bg ='silver', command=lambda:fitness_seminars(kind='recorded'))
@@ -220,7 +230,8 @@ elif(Type=="Basic"):
     kind='live'
     num3 = tk.Label(Client, text ="3.", font=('bold',20) )
     num3.place(x = 30, y = 150)
-    submitbtn3 = tk.Button(Client, text ="Enroll to "+kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='live'))
+    #submitbtn3 = tk.Button(Client, text ="Enroll to "+kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='live'))
+    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=training_sessions1)
     submitbtn3.place(x = 50, y = 150, width = 200)
     
     #access to recorded fitness seminars page
@@ -250,7 +261,8 @@ elif(Type=="Premium"):
     kind='recorded'
     num1 = tk.Label(Client, text ="1.", font=('bold',20) )
     num1.place(x = 30, y = 90)
-    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    #submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='recorded'))
+    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=training_sessions1)
     submitbtn1.place(x = 50, y = 90, width = 200)
     
     #access to recorded fitness seminars page
@@ -263,7 +275,8 @@ elif(Type=="Premium"):
     kind='live'
     num3 = tk.Label(Client, text ="3.", font=('bold',20) )
     num3.place(x = 30, y = 150)
-    submitbtn3 = tk.Button(Client, text ="Enroll to "+kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='live'))
+    #submitbtn3 = tk.Button(Client, text ="Enroll to "+kind+" Traning sessions",bg ='silver', command=lambda:train_sessions(kind='live'))
+    submitbtn1 = tk.Button(Client, text =kind+" Traning sessions",bg ='silver', command=training_sessions1)
     submitbtn3.place(x = 50, y = 150, width = 200)
     
     #access to recorded fitness seminars page
