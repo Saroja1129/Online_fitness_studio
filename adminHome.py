aa4f0d7d402f83d764c02e9bf97b705bdb5e3f85
vikastadepu <41760133+vikastadepu@users.noreply.github.com>
2022-05-01 02:10:41 -0700

    Update adminHome.py

--- adminHome.py
+++ adminHome.py
@@ -1,154 +1,148 @@
 import tkinter as tk
 import mysql.connector as mysql
 import tkinter.messagebox as msgbox
-
-
 from tkinter import *
 from subprocess import call
-from hashlib import pbkdf2_hmac # for hash function
-from PIL import *
+import argparse
 
-#Change here for your local DB password
-local_DB_password = "Arti@123"
-python_alias = "python"
+# pass current user information
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="Current user ")
+parser.add_argument("--pw", help="Local password for DB engine")
+args = parser.parse_args()
+user = args.input
+local_DB_Password = args.pw
 
+   
+def create_fitness_seminar():
+    call(["python","fitness_seminar.py"])    
 
-def Register():
-            Admin.destroy()
-            #call client Registration page instead of admin
-            call(["python","Register.py"])
-            return True
+def Fetch_Training_session():
+    Admin2 = tk.Tk()
+    Admin2.title("Admin")
+    Admin2.geometry("1300x900") 
+    my_connect = mysql.connect(host="localhost", user="root", passwd=local_DB_Password, database="fitnessstudio" )
+    connection = my_connect.cursor()
+    query = "select * from training_session"
 
-# Hash incoming passwords 
-def hashPassword(passw):
+    connection.execute(query)
 
-    iter = 500_000 
-
-    #Convert password (string) into byte
-    passw_b = str.encode(passw)
-    hash = pbkdf2_hmac('sha256', passw_b, b'salt'*2, iter)
-
-    return str(hash.hex())
-
-
-def submitact():
-      
-    user = Username.get()
-    passw = Password.get()
-    passw = hashPassword(passw)      
-    a=clicked.get()
-    
-
-    if(user=="" or passw==""):
-        msgbox.showinfo("Insert status","all fields are required")
-    elif(a=='Admin'):
-     
-        con=mysql.connect(host="localhost",user="root",password=local_DB_password, db="fitnessstudio") 
-        cursor=con.cursor()
-        cursor.execute("select * from admin where admin_email = %s and admin_pwd=%s", [(user),(passw)] )
-        results=cursor.fetchall()
-        if results:
-            msgbox.showinfo("Login status","lOGIN SUCCESSFULL")
-            Admin.destroy()
-            call([python_alias,"adminHome.py","--input", user, "--pw", local_DB_password ])
-            return True
-        else:
-            msgbox.showinfo("Login status","lOGIN UNSUCCESSFULL")
-            return False
+    myresult = connection.fetchall()
         
-    elif(a=='Client'):
-            # Update user and password
-        con=mysql.connect(host="localhost",user="root",password = local_DB_password,db="fitnessstudio") 
-        cursor=con.cursor()
-        cursor.execute("select * from client where client_email = %s and client_pwd =%s", [(user),(passw)] )
-        results=cursor.fetchall()
-        if results:
-            msgbox.showinfo("Login status","lOGIN SUCCESSFULL")
-            Admin.destroy()
-            #call client home_page instead of admin
-            call([python_alias,"client_home.py","--input", user, "--pw", local_DB_password ])
-            return True
-        else:
-            msgbox.showinfo("Login status","lOGIN UNSUCCESSFULL")
-            return False
+    # Create title within page
+    Label_Client = tk.Label(Admin2, text ="Seminar List" )
+    Label_Client.config(font=("Courier", 15))
+    Label_Client.place(x = 10, y = 20)
+
+  
+    i = 0
+    for client in myresult: 
+
+        
+        #Create underlying buttons for the name of clients
+        # createClientButtonN = tk.Button(Admin2, text =" Get Client ",
+        #                   bg ='white', borderwidth = 0, command=lambda:getSingleClient(myresult,client))
+        y1 = 50
+        y2 = y1+i*20
+
+        # createClientButtonN.place(x = 1150, y = y2, width = 80) 
+
+        for j in range(len(client)):
+            e = Entry(Admin2,width=20, fg='blue')
+            e.grid(row=i, column=j) 
+            e.insert(END, client[j])
+            x1 = 10+j*120
+            y1 = 50 +i*20
+            e.place(x = x1 , y = y1)      
+     
+        i=i+1
+
+
+    Admin2.mainloop()
+
+
+def create_training_session():
+    call(["python","Adding_training_session.py"]) 
     
-    elif(a=='Advisor'):
-            # Update user and password
-        con=mysql.connect(host="localhost",user="root",password = local_DB_password,db="fitnessstudio") 
-        cursor=con.cursor()
-        cursor.execute("select * from advisor where email = %s and password =%s", [(user),(passw)] )
-        results=cursor.fetchall()
-        if results:
-            msgbox.showinfo("Login status","lOGIN SUCCESSFULL")
-            Admin.destroy()
-            #call client home_page instead of admin
-            call([python_alias,"advisor_home.py","--input", user, "--pw", local_DB_password ])
-            return True
-        else:
-            msgbox.showinfo("Login status","lOGIN UNSUCCESSFULL")
-            return False
+def client_list():
+    call(["python","Admin_client_list.py"])     
     
-    elif(a=='Instructor'):
-             # Update user and password
-         con=mysql.connect(host="localhost",user="root",password = local_DB_password,db="fitnessstudio") 
-         cursor=con.cursor()
-         cursor.execute("select * from Instructor where email = %s and Password =%s", [(user),(passw)] )
-         results=cursor.fetchall()
-         if results:
-             msgbox.showinfo("Login status","lOGIN SUCCESSFULL")
-             Admin.destroy()
-             
-             call([python_alias,"Instructor.py","--input", user, "--pw", local_DB_password ]) # TODO call client home_page instead of admin
-             return True
-         else:
-             msgbox.showinfo("Login status","lOGIN UNSUCCESSFULL")
-             return False    
-                
-           
-            
-Admin=tk.Tk()
-Admin.title("Fitness_Studio")
-Admin.geometry("900x900")
+def membership():
+    def changed():
+        a=Mem_level.get()
+        b=Mem_cost.get()
+        print(a)
+        print(b)
+        m = mysql.connect(host="localhost", user="root", passwd=local_DB_Password, database="fitnessstudio" )
+        connection = m.cursor()
+        query = "update membership set mem_cost=(%s) where mem_level=(%s)",[(a),(b)]
+        connection.execute(query)
+        myresult = connection.fetchall()
+        m.commit()
+    
+    a=tk.Tk()
+    a.geometry("200x300")
+    Label_1 = tk.Label(a, text ="Membership level", )
+    Label_1.place(x=10, y=10)
 
-Label_IH = tk.Label(Admin, text ="Welcome To Online Fitness Studio" )
-Label_IH.config(font=("Courier", 30))
-Label_IH.place(x = 30, y = 20)
+    Mem_level = tk.Entry(a, width = 20)
+    Mem_level.place(x=10, y= 50)
+    
+    Label_2 = tk.Label(a, text ="Membership cost", )
+    Label_2.place(x=10, y= 100)
 
-Label_Domain = tk.Label(Admin, text ="Domain ", font=('bold',10) )
-Label_Domain.place(x = 200, y = 210)
-
-clicked=StringVar()
-clicked.set("Admin")
-drop = OptionMenu( Admin , clicked, "Admin","Instructor","Client","Advisor")
-drop.place(x = 300, y = 210, width = 200)
+    Mem_cost = tk.Entry(a, width = 20)
+    Mem_cost.place(x=10, y= 150)
+    
+    submitbtn1 = tk.Button(a, text ="Modify",bg ='silver', command=changed)
+    submitbtn1.place(x=10,y=200).pack()
+    a.mainloop()
+    
 
 
 
-Label_User_name = tk.Label(Admin, text ="UserName ", )
-Label_User_name.place(x = 200, y = 280)
 
-Username = tk.Entry(Admin, width = 35)
-Username.place(x = 300, y = 280, width = 200)
+Admin_home=tk.Tk()
+Admin_home.title("Admin_Home_Page")
+Admin_home.geometry("800x600")
 
-Label_Password = tk.Label(Admin, text ="Password ", )
-Label_Password.place(x = 200, y = 350)
+my_connect = mysql.connect(host="localhost", user="root", passwd=local_DB_Password, database="fitnessstudio" )
+connection = my_connect.cursor()
+query = "select Admin_name from Admin where Admin_Email = " +  "'" + str(user) + "'"
+connection.execute(query)
+results=connection.fetchall()
+a=results[0][0]
 
-Password= tk.Entry(Admin, width = 35)
-Password.place(x = 300, y = 350, width = 200)
-# Password.config(show="*")
+Label_IH = tk.Label(Admin_home, text ="Current Admin User-----")
+Label_IH.config(font=("Courier", 12))
+Label_IH.place(x = 10, y = 20)
+
+Label_IH1 = tk.Label(Admin_home, text =a )
+Label_IH1.config(font=("Courier", 12))
+Label_IH1.place(x = 150, y = 20)
+
+submitbtn = tk.Button(Admin_home, text ="Current Clients",
+                      bg ='gray', command=client_list)
+submitbtn.place(x = 150, y = 140, width = 300)
+
+submitbtn = tk.Button(Admin_home, text ="schedule fitness seminar",
+                      bg ='gray', command=create_fitness_seminar)
+submitbtn.place(x = 150, y = 240, width = 300)
+
+submitbtn = tk.Button(Admin_home, text ="Current Scheduled Training sessions",
+                      bg ='gray', command=Fetch_Training_session)
+submitbtn.place(x = 150, y = 340, width = 300)
+
+submitbtn = tk.Button(Admin_home, text ="Create group training session",
+                      bg ='gray', command=create_training_session)
+submitbtn.place(x = 150, y = 440, width = 300)
+
+submitbtn = tk.Button(Admin_home, text ="Change Membership cost",
+                      bg ='gray', command=membership)
+submitbtn.place(x = 150, y = 540, width = 300)
 
 
-submitbtn = tk.Button(Admin, text ="Login",
-                      bg ='blue', command=submitact)
-submitbtn.place(x = 350, y = 420, width = 55)
+Admin_home.mainloop()
 
-Label4 = tk.Label(Admin, text ="If you are new client", )
-Label4.place(x = 270, y = 500)
-
-submitbtn1 = tk.Button(Admin, text ="Register Here",
-                      bg ='silver', command=Register)
-submitbtn1.place(x = 400, y = 500, width = 100)
-
-Admin.mainloop()
 
 
