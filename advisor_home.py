bf41278dde42217cf24853f49cda823d9454797c
Kyra Foerster <kyra.foerster@web.de>
2022-04-30 21:16:28 -0700

    cleanup

--- advisor_home.py
+++ advisor_home.py
@@ -46,10 +46,7 @@ showClients = tk.Button(Advisor_home, text ="Show My Clients",
 showClients.place(x = 150, y = 140, width = 200)
 
 
-# Create Button Add New Client
-#showClients = tk.Button(Advisor_home, text ="Add New Client",
- #                     bg ='blue', command=addNewClient)
-#showClients.place(x = 400, y = 140, width = 200)
+
 
 # Run main loop
 Advisor_home.mainloop()
