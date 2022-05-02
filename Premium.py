71169c06934121c488fdb22c367d5f044477a016
VineetKiragi <37727735+VineetKiragi@users.noreply.github.com>
2022-05-02 11:47:06 -0700

    Updated with logging

--- Premium.py
+++ Premium.py
@@ -1,3 +1,4 @@
+# SJSU CMPE 138 Spring 2022 TEAM5
 import tkinter as tk
 import mysql.connector as mysql
 import tkinter.messagebox as msgbox
@@ -6,6 +7,16 @@ from tkinter import *
 from subprocess import call
 from PIL import *
 import argparse
+import logging
+
+# Create log file
+log_file = 'clientLog.txt'
+log_fh = logging.FileHandler(log_file)
+
+log_format = '%(asctime)s %(levelname)s: %(message)s'
+# Possible levels: DEBUG, INFO, WARNING, ERROR, CRITICAL    
+log_level = 'INFO' 
+logging.basicConfig(format=log_format, level=log_level,handlers=[log_fh])
 
 #pass current user informations
  
@@ -17,6 +28,7 @@ args = parser.parse_args()
 user = args.input
 #user="name3.last@gmail.com"
 python_alias=args.alias
+#python_alias='python'
 local_DB_password = args.pw
 #local_DB_password = "um41Tact$"
 
@@ -25,10 +37,25 @@ def BuyBasic():
     #print("We are in Buy Basic")
     con=mysql.connect(host="localhost",user="root",password=local_DB_password,db="fitnessstudio") 
     cursor=con.cursor()
-    cursor.execute("update membership set mem_level='Basic',mem_cost=350 where \
-           mem_client_id in (select client_id from client where client_email='" + str(user) +"')") 
-    con.commit()
-    msgbox.showinfo("Login status","YOU HAVE BEEN SUCCESSFULLY UPGRADED TO BASIC")
+    try:
+        #updating membership level to basic
+        query="update membership set mem_level='Basic',mem_cost=350 where \
+               mem_client_id in (select client_id from client where client_email='" + str(user) +"')"
+        logging.info(query) 
+        cursor.execute(query)
+        con.commit()
+        logging.info("Query was successful!")
+        msgbox.showinfo("Login status","YOU HAVE BEEN SUCCESSFULLY UPGRADED TO BASIC")
+        
+    except mysql.Error as err:
+        logging.error(err)
+        logging.error("Query not successful!")
+               
+               
+    #cursor.execute("update membership set mem_level='Basic',mem_cost=350 where \
+    #       mem_client_id in (select client_id from client where client_email='" + str(user) +"')") 
+    #con.commit()
+    #msgbox.showinfo("Login status","YOU HAVE BEEN SUCCESSFULLY UPGRADED TO BASIC")
     Premium.destroy()
     call([python_alias,"client_home.py","--input", user, "--pw",local_DB_password, "--alias", python_alias])
     #call([python_alias,"client_home.py","--input", user, "--pw", local_DB_password ])
@@ -38,10 +65,24 @@ def BuyPremium():
     #print("We are in Buy Premium")
     con=mysql.connect(host="localhost",user="root",password=local_DB_password,db="fitnessstudio") 
     cursor=con.cursor()
-    cursor.execute("update membership set mem_level='Premium',mem_cost=500 where \
-           mem_client_id in (select client_id from client where client_email='" + str(user) +"')") 
-    con.commit()
-    msgbox.showinfo("Login status","YOU HAVE BEEN UPGRADED SUCCESSFULLY TO PREMIUM")
+    try:
+        #update membership to premium
+        query="update membership set mem_level='Premium',mem_cost=500 where \
+               mem_client_id in (select client_id from client where client_email='" + str(user) +"')"
+        logging.info(query) 
+        cursor.execute(query)
+        con.commit()
+        logging.info("Query was successful!")
+        msgbox.showinfo("Login status","YOU HAVE BEEN UPGRADED SUCCESSFULLY TO PREMIUM")
+        
+    except mysql.Error as err:
+        logging.error(err)
+        logging.error("Query not successful!")
+               
+    #cursor.execute("update membership set mem_level='Premium',mem_cost=500 where \
+    #       mem_client_id in (select client_id from client where client_email='" + str(user) +"')") 
+    #con.commit()
+   # msgbox.showinfo("Login status","YOU HAVE BEEN UPGRADED SUCCESSFULLY TO PREMIUM")
     Premium.destroy()
     call([python_alias,"client_home.py","--input", user, "--pw",local_DB_password, "--alias", python_alias])
     #call([python_alias,"client_home.py","--input", user, "--pw", local_DB_password ])
